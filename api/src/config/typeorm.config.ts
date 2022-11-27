91a92c6e277eefdda69553043fd4ffb1102018a1
PapiHack <mssmbaye@gmail.com>
2022-11-27 17:54:12 +0000

    Dockerization of the REST API built with Nest.js

--- api/src/config/typeorm.config.ts
+++ api/src/config/typeorm.config.ts
@@ -5,8 +5,8 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export const TypeOrmConfiguration: TypeOrmModuleOptions = {
-  type: 'mysql',
-  // type: 'postgres',
+  // type: 'mysql',
+  type: 'postgres',
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   username: process.env.DB_USERNAME,
