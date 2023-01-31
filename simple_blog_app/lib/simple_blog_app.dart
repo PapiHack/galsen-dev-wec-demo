43e9663641b0f518143b608e641f232053cfdb69
PapiHack <mssmbaye@gmail.com>
2023-01-31 22:27:28 +0000

    Setup basic architecture and install some dependencies

---
+++ simple_blog_app/lib/simple_blog_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SimpleBlogApp extends StatelessWidget {
+  const SimpleBlogApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
