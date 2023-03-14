5fac4dedc9abafb664137fcf0cf491c8c4663bc6
PapiHack <mssmbaye@gmail.com>
2023-03-14 13:42:01 +0000

    Small refacto of article's repository and service

--- simple_blog_app/lib/services/article_service.dart
+++ simple_blog_app/lib/services/article_service.dart
@@ -3,22 +3,19 @@ import 'package:simple_blog_app/data/data.dart';
 import 'package:simple_blog_app/utils/utils.dart';
 
 class ArticleService {
-  final String? endpoint;
-  HttpClient? httpClient;
+  final String endpoint;
+  final HttpClient httpClient = HttpClient();
 
   ArticleService({
     this.endpoint = 'articles',
-    this.httpClient,
-  }) {
-    this.httpClient = this.httpClient ?? HttpClient();
-  }
+  });
 
   Future<Response<dynamic>> fetchAll() async =>
-      await this.httpClient!.get(this.endpoint!);
+      await this.httpClient.get(this.endpoint);
 
   Future<Response<dynamic>> fetchById(int articleID) async =>
-      await this.httpClient!.get('${this.endpoint}/$articleID');
+      await this.httpClient.get('${this.endpoint}/$articleID');
 
   Future<Response<dynamic>> createArticle(ArticleDTO articleDTO) async =>
-      await this.httpClient!.post(this.endpoint!, articleDTO.toJson());
+      await this.httpClient.post(this.endpoint, articleDTO.toJson());
 }
